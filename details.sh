97ca601a5444313e2718c05c7ab7049229d43007
testuser1 <testuser1@ip-10-1-1-224.ec2.internal>
2019-12-21 21:54:35 +0000

    Added Sudo code and users in the machine

--- details.sh
+++ details.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
 echo "This script will provide information about the local system"
+echo
+#GIVES HOSTNAME
 echo $(hostname)
+#GIVES USER EXECUTED THIS SCRIPT
+echo
 echo $(whoami)
-echo $(id -un)
-echo "Hi"
+echo
+#GIVES USERS IN THE SYSTEM
+echo
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+
+echo $USERS
+
